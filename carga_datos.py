a1540f458fd686c178610948abb8d2f7649be5f4
Denis <denisalmandoz@gmail.com>
2023-05-22 12:38:51 +0200

    juntar_csvs

--- carga_datos.py
+++ carga_datos.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 def cargador(ubicacion):
     if ubicacion[-4:] == ".csv":
@@ -14,3 +15,12 @@ def cargador(ubicacion):
 
 if __name__ == "__main__":
     datos = cargador("/home/laptop/Proyectos Python/LimpiezaDatos/laliga_22-23.csv")
+    
+# Cargar los conjuntos de datos y concatenarlos
+def juntar_csvs(path:str, files: list):
+    for file in files:
+        if file.endswith(".csv"):  # Solo cargar archivos con extensión .csv
+            path_file = os.path.join(path, file)
+            data = pd.read_csv(path_file)
+            df = pd.concat([df, data])
+    return (df)
