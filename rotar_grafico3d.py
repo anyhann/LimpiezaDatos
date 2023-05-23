068bcb78e648077f4801bb378dd9601e2ccdb3b4
david martin <dmartinsaiatuz2023@gmail.com>
2023-05-23 09:08:07 +0200

    Ok

--- rotar_grafico3d.py
+++ rotar_grafico3d.py
@@ -5,11 +5,11 @@ import numpy as np
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 
-x = np.linspace(-4,4,50)
+x = np.linspace(-4,4,50)  #"linspace" -> Es la escala de la cuadricula.
 y = np.linspace(-4,4,50)
 X, Y = np.meshgrid(x,y)
 def z(x,y):
-    return np.sin(np.sqrt(x**2+y**2))
-
-ax.contourf(X,Y,z(X,Y))
+    return np.sin(np.sqrt(x**2+y**2)) #Aquí hay que escribir la formula matematica: Seno.
+    return np.cos(np.sqrt(x**2+y**2)) #Aquí hay que escribir la formula matematica: Coseno.
+    return np.sin(np.sqrt(x**2+y**2)) #Aquí hay que escribir la formula matematica: Seno, Coseno, Tangente
 plt.show()
