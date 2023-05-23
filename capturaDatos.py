5027947ea5449a735059dbd37c7a404369845044
Aitor <aitordonado@gmail.com>
2023-05-23 12:29:31 +0200

    CapturaDatos actualizada

--- capturaDatos.py
+++ capturaDatos.py
@@ -1,21 +1,24 @@
 def leer_opciones_teclado():
     while True:
         opcion = input("Ingrese una opción: ")
-
         if opcion == "1":
             # Hacer algo para la opción 1
             print("Opción 1 seleccionada")
+            return opcion
         elif opcion == "2":
             # Hacer algo para la opción 2
             print("Opción 2 seleccionada")
+            return opcion
         elif opcion == "3":
             # Hacer algo para la opción 3
             print("Opción 3 seleccionada")
+            return opcion
         elif opcion == "q":
             # Salir del ciclo y devolver la opción seleccionada
-            return opcion
+            return "Ninguna"
         else:
             print("Opción inválida. Intente nuevamente.")
+
 
   
 
