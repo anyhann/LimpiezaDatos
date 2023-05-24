1316dde42c5f8000e7663f931581181e6a861908
Aitor <aitordonado@gmail.com>
2023-05-24 12:20:03 +0200

    Mejoras en selecciona_columnas

--- selecciona_columnas.py
+++ selecciona_columnas.py
@@ -11,11 +11,14 @@ def pregunta_opcion():
     eleccion =  leer_opciones_teclado()
     return eleccion
 
+def selecciona_columnas(datos):
+    opcion = pregunta_opcion()
+    if opcion == "1":
+        salida = datos[datos.describe().columns]
+        return salida
 
 if __name__ == "__main__":
     main_file_path = os.path.abspath(__file__)
     datos = cargador(os.path.join(os.path.dirname(main_file_path), "hormigon.csv"))
     opcion = pregunta_opcion()
-    if opcion == "1":
-        salida = datos[datos.describe().columns]
-        print(salida)
+    print(selecciona_columnas(datos))
