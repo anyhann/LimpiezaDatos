05b09a6e97603449b4c23ec6d4664da9bcfa4a49
Denis <denisalmandoz@gmail.com>
2023-05-24 12:36:52 +0200

    regresion_simple

--- calcular_R2_regresionlineal.py
+++ calcular_R2_regresionlineal.py
@@ -1,7 +1,7 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 
-def calcular_R2_regresion(X, y):
+def calcular_R2_regresion_simple(X, y):
 
     model = LinearRegression()
     model.fit(X, y)
