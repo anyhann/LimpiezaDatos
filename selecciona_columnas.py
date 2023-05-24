40123388c78dbb777c24f09a28ab03c003f669cd
Nerea85 <nereacb@gmail.com>
2023-05-24 13:25:44 +0200

    Añadida funcion para seleccionar columnas categoricas

--- selecciona_columnas.py
+++ selecciona_columnas.py
@@ -16,13 +16,21 @@ def selecciona_columnas(datos):
     if opcion == "1":
         salida = datos[datos.describe().columns]
         return salida
+    elif opcion == "3":
+        salida = selecciona_columnas_categoricas(datos)
+        return salida
 
 def selecciona_columnas_numericas(datos):
     salida = datos[datos.describe().columns]
     return salida
 
+def selecciona_columnas_categoricas(datos):
+    # Obtener columnas de tipo object
+    salida = datos.filter(datos.select_dtypes(include=['object']), axis=1)
+    return salida
+
 if __name__ == "__main__":
     main_file_path = os.path.abspath(__file__)
-    datos = cargador(os.path.join(os.path.dirname(main_file_path), "hormigon.csv"))
-    opcion = pregunta_opcion()
+    datos = cargador(os.path.join(os.path.dirname(main_file_path), "video_juegos_populares.csv"))
+    #opcion = pregunta_opcion()
     print(selecciona_columnas(datos))
