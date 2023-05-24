f8037183b9432702351320f1bca00602d68dc4bf
Denis <denisalmandoz@gmail.com>
2023-05-24 14:50:58 +0200

    comments_added

--- calcular_R2_regresionlineal.py
+++ calcular_R2_regresionlineal.py
@@ -1,17 +1,30 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 
-def calcular_R2_regresion(X, y):
-
-    model = LinearRegression()
-    model.fit(X, y)
-    y_pred = model.predict(X)
-    r2 = r2_score(y, y_pred)
-
-    return r2
-
-
-def calcular_R2_regresion_multiple(X, y):
-    X_train, X_train, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    modelo.fit(X_train, y_train)
+def calcular_R2_regresion(dataset, columna_objetivo):
     
+    #Nos quedamos con las variables numericas
+    dataset_num = dataset.select_dtypes(include=['number'])
+    
+    X = dataset_num.drop(columna_objetivo, axis=1)
+    y = dataset_num[columna_objetivo]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    modelo = LinearRegression()
+    modelo.fit(X_train, y_train)
+
+    train_score = modelo.score(X_train, y_train)
+    test_score = modelo.score(X_test, y_test)
+    predictions = modelo.predict(X_test)
+
+    # Grafico de valor real y valor predicho
+    plt.scatter(predictions, y_test)
+    plt.xlabel('Predicted Value')
+    plt.ylabel('Actual Value')
+    plt.show()
+
+    return train_score, test_score
+
+
