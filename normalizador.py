763c3c79618e060e5cec21b6b1f20f567a30e0dc
Aitor <aitordonado@gmail.com>
2023-05-25 11:57:30 +0200

    Introducido normalizador

---
+++ normalizador.py
@@ -0,0 +1,46 @@
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import StandardScaler
+
+
+def leer_opciones_teclado():
+    while True:
+        opcion = input("Ingrese una opción: ")
+        if opcion == "1":
+            # Hacer algo para la opción 1
+            print("Opción 1 seleccionada")
+            return opcion
+        elif opcion == "2":
+            # Hacer algo para la opción 2
+            print("Opción 2 seleccionada")
+            return opcion
+        elif opcion == "3":
+            # Hacer algo para la opción 3
+            print("Opción 3 seleccionada")
+            return opcion
+        elif opcion == "q":
+            # Salir del ciclo y devolver la opción seleccionada
+            return "Ninguna"
+        else:
+            print("Opción inválida. Intente nuevamente.")
+
+
+def normalizador(datos):
+    """
+    Normaliza los datos preguntando por el algoritmo de normalización más conveniente
+    """
+    print("""Normalizadores disponibles:
+    1. min-max scaler
+    2. Standard scaler
+    3. No disponible
+    q. Salir
+    """)
+    eleccion = leer_opciones_teclado()
+    if eleccion == "1":
+        scaler = MinMaxScaler()
+    elif eleccion == "2":
+        scaler = StandardScaler()
+    else:
+        print("Los datos no se han normalizado")
+        return datos
+    normalized_data = scaler.fit_transform(datos)
+    return normalized_data
