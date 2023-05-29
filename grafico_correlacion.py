b0668e30d72f1eee4bc86bbc796feb21a890dfac
Aitor <aitordonado@gmail.com>
2023-05-29 14:15:45 +0200

    Graf autocorrelacion

--- grafico_correlacion.py
+++ grafico_correlacion.py
@@ -1,9 +1,12 @@
 
 from statsmodels.graphics.tsaplots import plot_acf
 import matplotlib as plt
-
+from carga_datos import cargador
 
 def grafico_autocorrelacion (columna_objetivo, num_lags):
     fig, ax = plt.subplots(figsize=(7, 3))
     plot_acf(columna_objetivo, ax=ax, lags=num_lags)
     plt.show()
+
+if __name__ == "__main__":
+    datos = cargador("https://raw.githubusercontent.com/JoaquinAmatRodrigo/skforecast/master/data/vic_elec.csv")
