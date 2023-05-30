d332701f31974015ba69af0a64e0584961b2de50
Aitor <aitordonado@gmail.com>
2023-05-30 11:50:02 +0200

    Mod Serie Temporal

--- Serie_temporal.py
+++ Serie_temporal.py
@@ -1,8 +1,6 @@
 import pandas as pd
 from statsmodels.graphics.tsaplots import plot_acf
 import matplotlib as plt
-from sklearn.preprocessing import StandardScaler
-from carga_datos import cargador
 from carga_serie_temporal import auto_conversion_datetime
 
 
@@ -18,7 +16,7 @@ class SerieTemporal:
         dataframe.set_index(columna, inplace=True)
         return dataframe
 
-
+    
     def grafico_auto (self, columna_valor, num_lags):
         fig, ax = plt.subplots(figsize=(7, 3))
         plot_acf(columna_valor, ax=ax, lags=num_lags)
