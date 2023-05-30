edca417a370a8af1c8f7f69c122a09da941fea45
Aitor <aitordonado@gmail.com>
2023-05-30 12:28:38 +0200

    mod

--- carga_serie_temporal.py
+++ carga_serie_temporal.py
@@ -9,8 +9,8 @@ def conversion_a_serie_temp(dataframe, columna):
     # Convertir la columna de texto a datetime
     dataframe[columna] = auto_conversion_datetime(dataframe[columna])
     dataframe.set_index(columna, inplace=True)
-    datos = datos.asfreq(columna[1]-columna[0])
-    print("La frecuencia de la serie temporal es:", datos.index.freq)
+    dataframe = dataframe.asfreq(dataframe.index[1]-dataframe.index[0])
+    print("La frecuencia de la serie temporal es:", dataframe.index.freq)
     dataframe = dataframe.sort_index()
     return dataframe
 
