0cdfc17fd2b36516d70b70b028fd82d6e974e5da
david martin <dmartinsaiatuz2023@gmail.com>
2023-05-30 14:58:29 +0200

    completa nan

--- completar_nan_dataframe.py
+++ completar_nan_dataframe.py
@@ -1,14 +1,15 @@
 import pandas as pd
 from carga_datos import cargador
 
-def completa_nan (dataframe):
-# DataFrame de ejemplo
-    completa = pd.DataFrame({'A': [1, 2, None, 4],
-                   'B': [5, None, 7, None]})
+dataframe = pd.DataFrame({'A': [1, 2, None, 4], 'B': [5, None, 7, None]})
 
-# Reemplazar los NaN con la media de la columna
-dataframe.rolling(3, center=True).mean().head()
-print(completa_nan(dataframe))
+def completa_nan(dataframe):
+    # DataFrame de ejemplo
+    # Reemplazar los NaN con la media de la columna
+    dataframe.rolling(3, center=True).mean().head()
+
+
 
 if __name__ == "__main__":
+    print(completa_nan(dataframe))
     
