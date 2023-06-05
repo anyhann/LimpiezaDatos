fc106298e6cae23747abe204757c06388de0639c
Txomin <t.uriartesaiatuz2023@gmail.com>
2023-06-05 12:48:33 +0200

    mejora nansbool

--- completa_nansbool.py
+++ completa_nansbool.py
@@ -5,5 +5,5 @@ def rellenar_nulos_bool(columna):
                 if columna[i-1] == columna[i+1]:
                     columna[i] = columna[i-1]
             elif columna[i+1] is not None:
-                columna[i] = columna[i+1]
+                columna[i] = columna[i-1]
     return columna
