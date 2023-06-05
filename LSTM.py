778a0a96f9951552630cb941bb45577d31e14323
Aitor <aitordonado@gmail.com>
2023-06-05 14:06:48 +0200

    separación train val y grafica

--- LSTM.py
+++
@@ -1,8 +0,0 @@
-
-import matplotlib.pyplot as plt
-
-def grafico_datos_train_y_validacion_LSTM (set_entrenamiento,set_validacion, columna_objetivo):
-    set_entrenamiento[columna_objetivo].plot(legend=True)
-    set_validacion[columna_objetivo].plot(legend=True)
-    plt.legend(['Entrenamiento', 'Validación'])
-    plt.show()
--- grafico_datos_train_y_validacion.py
+++ grafico_datos_train_y_validacion.py
@@ -1,16 +1,22 @@
 import matplotlib as plt
 
 #Función que separa los datos del train y del test en un grafico
-def grafico_sets(dataframe, columna_tiempo, fecha_inicio_validacion, columna_valores):
+def separa_train_val(self, fecha_inicio_validacion):
+    dataframe = self.dataframe
     posicion = dataframe.index.get_loc(fecha_inicio_validacion)
+
+    self.set_entrenamiento = dataframe.iloc[:posicion]
+    self.set_validacion = dataframe.iloc[posicion:]
+    grafico_sets(self)
     
-    set_entrenamiento = dataframe[columna_valores].iloc[:posicion-1]
-    set_validacion = dataframe[columna_valores].iloc[posicion:]
-    
-    set_entrenamiento.plot(legend=True)
-    set_validacion.plot(legend=True)
-    
+def grafico_sets(self):  
+    self.set_entrenamiento[self.columna_valores].plot(legend=True)
+    self.set_validacion[self.columna_valores].plot(legend=True)
     plt.legend(['Entrenamiento', 'Validación'])
     plt.show()
+
+
+
+
 
 
