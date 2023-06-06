0b0928fc1da686d631894d9997c439a57ccd9e8a
Nerea85 <nereacb@gmail.com>
2023-06-06 13:16:16 +0200

    Validacion de los parametros introducidos por el usuario

--- ohencoder_clase.py
+++ ohencoder_clase.py
@@ -6,7 +6,16 @@ from captura_opciones import leer_opciones_pantalla
 
 class DataFrameTransformer():
     def __init__(self, dataframe):
-        self.df = dataframe
+        self.df = dataframe    
+
+    def isSubset(self, arr, subarr):
+        check_items = np.full_like(subarr, False)
+        
+        for idx, x in enumerate(subarr):
+            check_items[idx] = x in arr 
+
+        return all(check_items)
+		
 
     def mostrar_cols_categoricas(self):
 
@@ -43,13 +52,24 @@ class DataFrameTransformer():
         if opcion_seleccionada == "q":
             return
 
-        cols_seleccionadas = input("Introduce los índices de las columnas sobre las que quieres aplicar el encoding, separadas por coma: ")
-        
-        # Mostramos los nombres de las columnas seleccionadas por el usuario.
-        # Las columnas en el dataframe empiezan en 0 y los indices de las columnas que hemos mostrado por
-        # pantalla empiezan en 1 por lo que tenemos que restar 1 al indice seleccionado para acceder a 
-        # la columna correcta.
-        indices_seleccionados = [int(index) - 1 for index in cols_seleccionadas.split(",")]
+        resultado_validar = False
+        while resultado_validar == False:
+            cols_seleccionadas = input("Introduce los índices de las columnas sobre las que quieres aplicar el encoding, separadas por coma: ")
+            
+            # Mostramos los nombres de las columnas seleccionadas por el usuario.
+            # Las columnas en el dataframe empiezan en 0 y los indices de las columnas que hemos mostrado por
+            # pantalla empiezan en 1 por lo que tenemos que restar 1 al indice seleccionado para acceder a 
+            # la columna correcta.
+            indices_seleccionados_usuario = [int(index) for index in cols_seleccionadas.split(",")]
+            indices_seleccionados = [int(index) - 1 for index in cols_seleccionadas.split(",")]
+
+            # Validar que los indices introducidos por el usuario son validos.
+            indices_columnas_categoricas = list(dict_categoricas.keys())
+            resultado_validar = self.isSubset(indices_columnas_categoricas, indices_seleccionados_usuario)
+
+            if (not resultado_validar):
+                print("Error: Algunos valores no coinciden con los índices de las columnas disponibles.")
+       
 
         columnas_seleccionadas_nombres = [self.df.columns[idx] for idx in indices_seleccionados]
         str_columnas_seleccionadas = ", ".join(columnas_seleccionadas_nombres)
@@ -66,42 +86,6 @@ class DataFrameTransformer():
             return transformed_df
         else:
             return
-
-    def transform_dataframe(self):
-        if self.df is None:
-            print("Error: No se ha cargado ningún DataFrame.")
-            return None
-        
-       # Mostrar las columnas del DataFrame al usuario
-        print("Columnas disponibles:")
-        for col in self.df.columns:
-            print(col)
-        
-        # Solicitar al usuario que elija una columna por teclado
-        columna_elegida = input("Escribe el nombre de la columna que deseas seleccionar: ")
-        
-        # Mostrar los valores únicos de la columna seleccionada al usuario
-        valores_columna = self.df[columna_elegida].unique()
-        print("Valores únicos de la columna seleccionada:")
-        for val in valores_columna:
-            print(val)
-        
-        # Solicitar al usuario que elija el orden de los valores por orden jerárquico
-        print("Escribe los valores en el orden jerárquico deseado separados por comas:")
-        orden_valores = input().split(",")
-
-        # Verificar si todos los valores únicos están presentes en el orden jerárquico
-        if set(valores_columna) != set(orden_valores):
-            print("Error: Algunos valores no coinciden con el orden jerárquico.")
-            return self.df
-        
-        # Aplicar la transformación OrdinalEncoder a la lista de valores seleccionados
-        encoder = OrdinalEncoder(categories=[orden_valores])
-        valores_transformados = encoder.fit_transform(self.df[[columna_elegida]])
-        
-        # Crear una nueva columna en el DataFrame con los valores transformados
-        self.df['Valores Transformados'] = valores_transformados
-        return self.df
 
 
     def crea_dummies(self, columnas_seleccionadas): 
@@ -124,12 +108,21 @@ class DataFrameTransformer():
             valores_columnas = self.df[i].unique()
             print(valores_columnas)
 
-            # Solicitar al usuario que elija el orden de los valores por orden jerárquico
-            print("Escribe los valores en el orden jerárquico deseado separados por comas:")
-            orden_valores = input().split(",")
-            lista_categorias_todas_cols.append(orden_valores)
+            datos_ok = False
+            while datos_ok == False:
+                # Solicitar al usuario que elija el orden de los valores por orden jerárquico
+                print("Escribe los valores en el orden jerárquico deseado separados por comas:")
+                orden_valores = input().split(",")
+                lista_categorias_todas_cols.append(orden_valores)
 
-            # TODO control de errores
+                # Eliminar espacios en blanco alrededor de los valores ingresados
+                orden_valores = [valor.strip() for valor in orden_valores]
+
+                # Verificar si todos los valores únicos están presentes en el orden jerárquico
+                if set(valores_columnas) != set(orden_valores):
+                    print("Error: Algunos valores no coinciden con el orden jerárquico.")
+                else:
+                    datos_ok = True
 
         # Aplicar la transformación OrdinalEncoder a la lista de valores seleccionados
         encoder = OrdinalEncoder(categories=lista_categorias_todas_cols)
