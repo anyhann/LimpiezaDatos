c908caa7054ee8633dc8527769c1661ff8fa0cee
Aitor <aitordonado@gmail.com>
2023-06-14 14:06:30 +0200

    Clasificador

--- Clasificador.py
+++ Clasificador.py
@@ -5,16 +5,6 @@ from carga_datos import cargador
 from descripciones import descripcion
 
 
-def mod_clasificacion(datos):
-    descripcion_data = datos.describe(include="all").T
-    print(descripcion_data)
-    datos.isnull().sum()
-    datos.info()
-    if  not datos.isnull().any().any():
-        print("No hay valores nulos")
-    cuenta_en_cols(datos)
-    datos.dtypes
-    return
 
 def cuenta_en_cols(dataframe):
     columnas = dataframe.columns
@@ -22,18 +12,6 @@ def cuenta_en_cols(dataframe):
         cuenta_valores_unicos = dataframe[columna].value_counts()
         print(f"La columna {columna} tiene {cuenta_valores_unicos} valores únicos")
 
-def separa_train(self, porcentaje):
-    dataframe= self.dataframe
-    columna_clase = self.columna_clase
-    porcentaje = input("¿Qué porcentaje de las muestras deseas para el test: ")
-    corte = int(int(porcentaje)*dataframe.shape[0]/100)
-    train_data = dataframe[:-corte]
-    test_data = dataframe[corte:]
-    train_X = train_data.drop([columna_clase], axis=1)
-    train_y = train_data[columna_clase]
-    test_X = test_data.drop([columna_clase], axis=1)
-    test_y = test_data[columna_clase]
-    return train_X, train_y, test_X, test_y
 
 #Creamos el modelo
 from sklearn.linear_model import LogisticRegression
@@ -52,9 +30,7 @@ RegLog = LogisticRegression(C=1.0,
                             verbose=0,
                             warm_start=False)
 
-#Aplicamos el modelo a los datos de train.
-RegLog.fit(separa_train)
-RegLog.coef_
+
 
 class Clasificador:
     def __init__(self, dataframe, columna_clase):
@@ -71,20 +47,18 @@ class Clasificador:
         corte = int(int(porcentaje)*dataframe.shape[0]/100)
         train_data = dataframe[:-corte]
         test_data = dataframe[corte:]
-        train_X = train_data.drop([columna_clase], axis=1)
-        train_y = train_data[columna_clase]
-        test_X = test_data.drop([columna_clase], axis=1)
-        test_y = test_data[columna_clase]
-        self.train_X=train_X
-        self.train_y=train_y
-        self.test_X=test_X
-        self.test_y=test_y
-        return train_X, train_y, test_X, test_y
+        self.train_X = train_data.drop([columna_clase], axis=1)
+        self.train_y = train_data[columna_clase]
+        self.test_X = test_data.drop([columna_clase], axis=1)
+        self.test_y = test_data[columna_clase]
 
 
 
 
 if __name__ == "__main__":
     main_file_path = os.path.abspath(__file__)
-    datos = cargador(os.path.join(os.path.dirname(main_file_path), "datos", "hormigon.csv"))
+    datos = cargador(os.path.join(os.path.dirname(main_file_path), "datos", "winequality-red.csv"))
     print(descripcion(datos))
+    clasificador = Clasificador(datos, "quality")
+    clasificador.separa_train(porcentaje=33)
+    
