24d579a0a64fc544895133e58ed0a181e5860f7a
Aitor <aitordonado@gmail.com>
2023-06-20 13:38:05 +0200

    clasificador

--- Clasificador.py
+++ Clasificador.py
@@ -1,7 +1,21 @@
 import os
+import pandas as pd
+
+# MODELOS DE CLASIFICACIÓN
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score, confusion_matrix
+from xgboost import XGBClassifier
+from sklearn.ensemble import AdaBoostClassifier, GradientBoostingClassifier, RandomForestClassifier
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
+
 from carga_datos import cargador
 from descripciones import descripcion
-from sklearn.preprocessing import OneHotEncoder
+
 
 
 def cuenta_en_cols(dataframe):
@@ -31,37 +45,54 @@ RegLog = LogisticRegression(C=1.0,
                             verbose=0,
                             warm_start=False)
 
-
-
 class Clasificador:
     def __init__(self, dataframe, columna_clase):
         self.dataframe = dataframe
         self.columna_clase = columna_clase
 
     def separa_train(self, porcentaje=20):
-        dataframe= self.dataframe
-        columna_clase = self.columna_clase
         if porcentaje == 20:
             porcentaje = input("¿Qué porcentaje de las muestras deseas para el test? (por defecto 20):")
         if porcentaje == "":
             porcentaje = 20
-        corte = int(int(porcentaje)*dataframe.shape[0]/100)
-        train_data = dataframe[:-corte]
-        test_data = dataframe[corte:]
-        self.train_X = train_data.drop([columna_clase], axis=1)
-        self.train_y = train_data[columna_clase]
-        self.test_X = test_data.drop([columna_clase], axis=1)
-        self.test_y = test_data[columna_clase]
+        X = self.dataframe.drop([self.columna_clase], axis=1)
+        y = self.dataframe[self.columna_clase]
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=porcentaje/100, random_state=42)
 
-  # Aplicar One-Hot Encoding
-        encoder = OneHotEncoder(sparse=False)
-        encoded_train_X = encoder.fit_transform(train_data.drop([columna_clase], axis=1))
-        encoded_test_X = encoder.transform(test_data.drop([columna_clase], axis=1))
 
-        self.train_X = encoded_train_X
-        self.train_y = train_data[columna_clase]
-        self.test_X = encoded_test_X
-        self.test_y = test_data[columna_clase]
+    def comparar_modelos(self):
+        self.separa_train()
+        X_train, X_test, y_train, y_test = self.X_train, self.X_test, self.y_train, self.y_test
+
+        models = [
+            DecisionTreeClassifier(random_state=0),
+            RandomForestClassifier(random_state=0),
+            KNeighborsClassifier(),
+            SVC(random_state=0),
+            LogisticRegression(random_state=0),
+            XGBClassifier(),
+            AdaBoostClassifier(),
+            GradientBoostingClassifier(),
+        ]
+        models_comparison = {}
+
+        for model in models:
+            print(f"Modelo: {str(model)}\n")
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            accuracies = cross_val_score(estimator=model, X=X_train, y=y_train, cv=5)
+            print(classification_report(y_test, y_pred))
+            print("-" * 30, "\n")
+            models_comparison[f"{str(model)}"] = [
+                accuracy_score(y_pred, y_test),
+                f1_score(y_pred, y_test, average="macro"),
+                precision_score(y_pred, y_test, average="macro"),
+                recall_score(y_pred, y_test, average="macro"),
+                (accuracies.mean()),
+            ]
+
+        return models_comparison
+
 
 
 if __name__ == "__main__":
@@ -70,4 +101,6 @@ if __name__ == "__main__":
     print(descripcion(datos))
     clasificador = Clasificador(datos, "quality")
     clasificador.separa_train(porcentaje=33)
+    modelos = clasificador.comparar_modelos()
+    print(modelos)
     
